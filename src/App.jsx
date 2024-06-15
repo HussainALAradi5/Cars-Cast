55f8c113dd8c3ad880b30c1db9e6549479d8edc8
Sara Alalawi <sarah.alalawi92@gmail.com>
2024-06-16 02:44:58 +0300

    NOTE: some commits are only for the purpose of making a pull request. No Major changes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import Car from './components/Car'
+import CarDetails from './components/CarDetails'
+import Home from './components/Home'
+import Login from './components/Login'
+import Maps from './components/Maps'
+import Nav from './components/Nav'
+import Profile from './components/Profile'
+import Register from './components/Register'
+import Reviews from './components/Reviews'
+import Spell from './components/Spell'
+import Weather from './components/Weather'
+
 import './App.css'
 import Home from './pages/Home'
 const App = () => {
-  return (
-    <div>
-      <Home />
-    </div>
-  )
+  return <div></div>
 }
 
 export default App
