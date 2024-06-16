f06f6e2b8e05f98585eefde64811040c1331e278
Sara Alalawi <sarah.alalawi92@gmail.com>
2024-06-16 03:40:21 +0300

    imported useState and useEffect Hooks in the Home.jsx component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,10 @@ import CarDetails from '../components/CarDetails'
 import Nav from '../components/Nav'
 import '../home.css'
 import '../App.css'
+import { useEffect, useState } from 'react'
 const Home = () => {
+  const [cars, setCars] = useState([])
+
   return (
     <div className="homePage">
       <h1 className="homeHeader">welcome to our rental car project!</h1>
