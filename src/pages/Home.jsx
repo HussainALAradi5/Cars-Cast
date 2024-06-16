10aa23b9e219ea70f8b8e1d64d88751d4284ac15
Sara Alalawi <sarah.alalawi92@gmail.com>
2024-06-16 03:43:48 +0300

    initialized a state variable for cars

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,13 @@ import { useEffect, useState } from 'react'
 const Home = () => {
   const [cars, setCars] = useState([])
 
+  useEffect(() => {
+    const getCars = async () => {
+      try {
+      } catch {}
+    }
+  }, [])
+
   return (
     <div className="homePage">
       <h1 className="homeHeader">welcome to our rental car project!</h1>
