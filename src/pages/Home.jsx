588f25879299e3d1f5b76bad6dfb11ad50bd3080
Sara Alalawi <sarah.alalawi92@gmail.com>
2024-06-16 03:48:54 +0300

    initialize a state variable for search input field

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,16 @@ import Nav from '../components/Nav'
 import '../home.css'
 import '../App.css'
 import { useEffect, useState } from 'react'
+import axios from 'axios'
 const Home = () => {
   const [cars, setCars] = useState([])
+  const [searchResults, setSearchResults] = useState([])
 
   useEffect(() => {
     const getCars = async () => {
       try {
+        const response = await axios.get(`https://freetestapi.com/api/v1/cars`)
+        setCars()
       } catch {}
     }
   }, [])
