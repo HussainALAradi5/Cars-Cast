8d132f30c2d0fced13cac19e3e2deab3ec3013b5
hussain <hussainaradi.ha@gmail.com>
2024-06-18 18:20:19 +0300

    some fixes

--- client/src/components/Car.jsx
+++ client/src/components/Car.jsx
@@ -1,20 +1,26 @@
 import { Link } from 'react-router-dom'
 
 const Car = (props) => {
+  const id = props.id
   return (
-    <
-   <div className="card" onClick={props.onClick}>
-   <div className="img-wrapper">
-    <img src={props.image} alt={props.make}{}{props.model}/>
-   </div>
-   <div className="info-wrapper  flex-col">
-    <h3>{props.make}{}{props.model}</h3>
-    <p><strong>{props.price}</strong></p>
-    <p>//reviews link to be added</p>
-    
-     </div>
-
-   </div>
+    <Link to={`/car/${id}`}>
+      <div className="card" onClick={props.onClick}>
+        <div className="img-wrapper">
+          <img src={props.image} alt={`${props.make}-${props.model}`} />
+        </div>
+        <div className="info-wrapper  flex-col">
+          <h3>
+            {props.make}
+            {}
+            {props.model}
+          </h3>
+          <p>
+            <strong>{props.price}</strong>
+          </p>
+          <p>//reviews link to be added</p>
+        </div>
+      </div>
+    </Link>
   )
 }
 
