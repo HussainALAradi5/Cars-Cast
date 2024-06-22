8cc593a0f04ac6e9107cf9777db143b72d4995fc
Sara Alalawi <sarah.alalawi92@gmail.com>
2024-06-22 17:53:26 +0300

    updated backened profile routes

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const userCtrl = require('../controllers/user')
 const router = express.Router()
 
 router.get('/:id/edit', userCtrl.edit)
-router.put('/:id', userCtrl.update)
+router.put('/profile/:id', userCtrl.update)
 router.get('/:id', userCtrl.show)
 router.delete('/:id', userCtrl.remove)
 router.post('/register', userCtrl.create)
