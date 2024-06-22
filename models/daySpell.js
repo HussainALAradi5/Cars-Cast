c2d958e45537dd3d776cb6ecb48fc8a0859f10cf
hussain <hussainaradi.ha@gmail.com>
2024-06-22 10:39:17 +0300

    some minor changes to the structure

--- models/daySpell.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const daySpellSchema = new Schema(
-  { id: String, name: String, description: String },
-  { timestamps: true }
-)
-module.exports = mongoose.model('DaySpell', daySpellSchema)
